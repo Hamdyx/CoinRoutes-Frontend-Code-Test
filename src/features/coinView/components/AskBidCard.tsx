91f4e61c3b1dfe264427389dacbc8c763a0490fb
hamdyx <a.hamdy.nig@gmail.com>
2024-08-06 00:52:52 +0300

    style(general): :lipstick: add shared `NumberValueContainer`
    - refactored `AskBidCard` values with the shared component

--- src/features/coinView/components/AskBidCard.tsx
+++ src/features/coinView/components/AskBidCard.tsx
@@ -1,9 +1,7 @@
-import { Divider, Flex, Skeleton, Typography } from 'antd';
+import { Divider, Flex } from 'antd';
 
-import { formatNumber } from '@/utils/formatNumbers';
 import StyledCard from '@/components/shared/StyledCard';
-
-const { Title, Text } = Typography;
+import NumberValueContainer from '@/components/shared/NumberValueContainer';
 
 type Props = {
   cardTitle: string;
@@ -18,29 +16,14 @@ function AskBidCard({ cardTitle, price, priceTitle, quantity, quantityTitle, bac
   return (
     <StyledCard title={cardTitle} background={background}>
       <Flex justify="space-between" gap={8}>
-        <Flex vertical flex={1}>
-          {price === undefined ? <Skeleton.Input /> : <Title level={4}>{formatNumber(price)}</Title>}
-          <Text>{priceTitle}</Text>
-        </Flex>
+        <NumberValueContainer value={price} valueTitle={priceTitle} />
         <Divider
           type="vertical"
           style={{
             height: 'auto',
           }}
         />
-        <Flex vertical flex={1}>
-          {quantity === undefined ? (
-            <Skeleton.Input />
-          ) : (
-            <Title level={4}>
-              {formatNumber(quantity, {
-                minimumFractionDigits: 0,
-                maximumFractionDigits: 8,
-              })}
-            </Title>
-          )}
-          <Text>{quantityTitle}</Text>
-        </Flex>
+        <NumberValueContainer value={quantity} valueTitle={quantityTitle} />
       </Flex>
     </StyledCard>
   );
