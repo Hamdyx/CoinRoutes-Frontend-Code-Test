8a69e7b578831549fce7f3fd22602219539dcb89
hamdyx <a.hamdy.nig@gmail.com>
2024-08-06 03:50:45 +0300

    refactor(widget): :art: add `LadderTableRow` to handle the styled orderbook row
    - update table item key to include the type - update `getRowStyle` to get type from key

--- src/features/coinView/components/LadderTable.tsx
+++ src/features/coinView/components/LadderTable.tsx
@@ -4,8 +4,8 @@ import { useCoinPairStore } from '@/stores/coinPair';
 import { StyledTable } from '@/lib/theme/components/Table';
 import { skeletonsLoading } from '@/utils/skeletonsLoading';
 
-import { getRowStyle } from '../utils/getRowStyle';
 import { ladderTableColumns } from '../utils/ladderTableColumns';
+import LadderTableRow from './LadderTableRow';
 
 type Props = {
   type: 'bids' | 'asks';
@@ -22,9 +22,9 @@ function LadderTable({ type, dataArr, showHeader = true }: Props) {
         const [price, market_size] = el;
 
         return {
-          // * sets the key as the percentage to the total size to style the row background
+          // * sets the key as the type & percentage to the total size to style the row background
           // * add the index to the key to keep it unique
-          key: `${i}-${+market_size / +pairTicker?.last_size}`,
+          key: `${i}-${type}-${+market_size / +pairTicker?.last_size}`,
           market_size,
           price,
           my_size: 0,
@@ -32,24 +32,12 @@ function LadderTable({ type, dataArr, showHeader = true }: Props) {
       });
     } else return [];
   }, [dataArr, pairTicker?.last_size]);
+
   const tableColumns = ladderTableColumns(type, dataSource?.length === 0);
 
   const components = {
     body: {
-      row: ({ children, ...restProps }: any) => {
-        const record = restProps['data-row-key'];
-        return (
-          <tr
-            {...restProps}
-            style={{
-              //* if record type is not string means it's the dummy data for skeleton
-              background: typeof record === 'string' ? getRowStyle(record, type) : '',
-            }}
-          >
-            {children}
-          </tr>
-        );
-      },
+      row: LadderTableRow,
     },
   };
 
