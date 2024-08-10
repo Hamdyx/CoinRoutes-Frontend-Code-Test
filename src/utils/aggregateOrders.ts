86a09eb2464e19fbcc0d14f39ff0e97bbed5d004
hamdyx <a.hamdy.nig@gmail.com>
2024-08-11 02:51:26 +0300

    fix(widget): :adhesive_bandage: limit calculations of orders size up to 30 items

--- src/utils/aggregateOrders.ts
+++ src/utils/aggregateOrders.ts
@@ -5,8 +5,9 @@ import { roundPrice } from './roundPrice';
 export const aggregateOrders = (orders: Order[], aggregator: number): AggregatedOrders => {
   const aggregated: Record<string, number> = {};
   let totalSize = 0;
-  orders.forEach(([price, size]) => {
-    totalSize += parseFloat(size);
+  orders.forEach(([price, size], i) => {
+    //* calculate size up to 30 orders as we display only 15 orders in ladder view
+    if (i <= 30) totalSize += parseFloat(size);
     const roundedPrice = roundPrice(parseFloat(price), aggregator).toFixed(2);
     if (aggregated[roundedPrice]) {
       aggregated[roundedPrice] += parseFloat(size);
